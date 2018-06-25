a4a01e9c21ef7734ebcb47afda337bd246aa9f59
nyarmith <powah.serge@gmail.com>
2018-06-24 23:44:02 -0400

    Contact page added

---
+++ sub.js
@@ -0,0 +1,4 @@
+//this is literally just here to obfuscate against spam
+let fstPartMail="powah.serge"
+let sndPartMail="gmail.com"
+document.getElementById("contact").href="mailto:"+fstPartMail+"@"+sndPartMail;
